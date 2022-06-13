209b57365471173704d621716b720a3474b3c15d
Volodymyr Kravchenko <vlodya@gmail.com>
2022-06-13 15:58:32 +0300

    Update homework_12.py

--- homework_12.py
+++ homework_12.py
@@ -82,3 +82,205 @@ class Contacts:
 		
 # 05.
 
+import pickle
+
+
+class Person:
+    def __init__(self, name: str, email: str, phone: str, favorite: bool):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.favorite = favorite
+
+
+class Contacts:
+    def __init__(self, filename: str, contacts: list[Person] = None):
+        if contacts is None:
+            contacts = []
+        self.filename = filename
+        self.contacts = contacts
+        self.count_save = 0
+
+    def save_to_file(self):
+        with open(self.filename, "wb") as file:
+            pickle.dump(self, file)
+
+    def read_from_file(self):
+        with open(self.filename, "rb") as file:
+            content = pickle.load(file)
+        return content
+
+    def __getstate__(self):
+        attributes = self.__dict__.copy()
+        attributes['count_save'] = self.count_save + 1
+        return attributes
+		
+		
+# 06.
+
+import pickle
+
+
+class Person:
+    def __init__(self, name: str, email: str, phone: str, favorite: bool):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.favorite = favorite
+
+
+class Contacts:
+    def __init__(self, filename: str, contacts: list[Person] = None):
+        if contacts is None:
+            contacts = []
+        self.filename = filename
+        self.contacts = contacts
+        self.count_save = 0
+        self.is_unpacking = False
+
+    def save_to_file(self):
+        with open(self.filename, "wb") as file:
+            pickle.dump(self, file)
+
+    def read_from_file(self):
+        with open(self.filename, "rb") as file:
+            content = pickle.load(file)
+        return content
+
+    def __getstate__(self):
+        attributes = self.__dict__.copy()
+        attributes["count_save"] = attributes["count_save"] + 1
+        return attributes
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+        self.is_unpacking = True
+		
+		
+# 07.
+
+import copy
+
+
+class Person:
+    def __init__(self, name: str, email: str, phone: str, favorite: bool):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.favorite = favorite
+
+
+def copy_class_person(person):
+    return copy.copy(person)
+	
+	
+# 08.
+
+import copy
+import pickle
+
+
+class Person:
+    def __init__(self, name: str, email: str, phone: str, favorite: bool):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.favorite = favorite
+
+
+def copy_class_person(person):
+    return copy.copy(person)
+
+
+class Contacts:
+    def __init__(self, filename: str, contacts: list[Person] = None):
+        if contacts is None:
+            contacts = []
+        self.filename = filename
+        self.contacts = contacts
+        self.is_unpacking = False
+        self.count_save = 0
+
+    def save_to_file(self):
+        with open(self.filename, "wb") as file:
+            pickle.dump(self, file)
+
+    def read_from_file(self):
+        with open(self.filename, "rb") as file:
+            content = pickle.load(file)
+        return content
+
+    def __getstate__(self):
+        attributes = self.__dict__.copy()
+        attributes["count_save"] = attributes["count_save"] + 1
+        return attributes
+
+    def __setstate__(self, value):
+        self.__dict__ = value
+        self.is_unpacking = True
+
+
+def copy_class_contacts(contacts):
+    return copy.deepcopy(contacts)    
+	
+	
+# 09.
+
+import copy
+import pickle
+
+
+class Person:
+    def __init__(self, name: str, email: str, phone: str, favorite: bool):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.favorite = favorite
+
+    def __copy__(self):
+        new_person = self.__class__
+        result = new_person.__new__(new_person)
+        result.__dict__.update(self.__dict__)
+        return result
+
+
+class Contacts:
+    def __init__(self, filename: str, contacts: list[Person] = None):
+        if contacts is None:
+            contacts = []
+        self.filename = filename
+        self.contacts = contacts
+        self.is_unpacking = False
+        self.count_save = 0
+
+    def save_to_file(self):
+        with open(self.filename, "wb") as file:
+            pickle.dump(self, file)
+
+    def read_from_file(self):
+        with open(self.filename, "rb") as file:
+            content = pickle.load(file)
+        return content
+
+    def __getstate__(self):
+        attributes = self.__dict__.copy()
+        attributes["count_save"] = attributes["count_save"] + 1
+        return attributes
+
+    def __setstate__(self, value):
+        self.__dict__ = value
+        self.is_unpacking = True
+
+    def __copy__(self):
+        new_contacts = self.__class__
+        result = new_contacts.__new__(new_contacts)
+        result.__dict__.update(self.__dict__)
+        return result
+
+    def __deepcopy__(self, memo):
+        new_contacts = self.__class__
+        result = new_contacts.__new__(new_contacts)
+        memo[id(self)] = result
+        for k, v in self.__dict__.items():
+            setattr(result, k, copy.deepcopy(v, memo))
+        return result
